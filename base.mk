20621f710015a183afda60be31c7e78d99679e2f
mistercheese <quesoo@gmail.com>
2016-07-08 15:20:10 +0200

    tissot: add glove mode from sony's huashan

--- base.mk
+++ base.mk
@@ -15,6 +15,12 @@ $(call inherit-product, vendor/hardware_overlay/overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Glove mode
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
+
+
 #Those overrides are here because Huawei's init read properties
 #from /system/etc/prop.default, then /vendor/build.prop, then /system/build.prop
 #So we need to set our props in prop.default
