4055e91911b9b5c4b72764f170dcd8e7adc44f3f
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-05-26 18:02:25 -0300

    Cleanup arm targets

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,3 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_arm64_a.mk \
-	$(LOCAL_DIR)/aosp_arm64_ab.mk \
-	$(LOCAL_DIR)/aosp_arm_a.mk \
-	$(LOCAL_DIR)/aosp_arm_ab.mk
+	$(LOCAL_DIR)/aosp_arm64_ab.mk
