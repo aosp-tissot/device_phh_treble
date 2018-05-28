24e369f269f80c54f46da822eb5917e5d64a00b9
sooti <sooti85@gmail.com>
2018-05-28 12:38:44 +0000

    we are aosp

---
+++ aosp_arm64_bvN.mk
@@ -0,0 +1,2 @@
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
--- treble_arm64_boN.mk
+++ treble_arm64_boN.mk
@@ -1,18 +1,13 @@
-TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/treble_common.mk
 $(call inherit-product, vendor/vndk/vndk.mk)
 $(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, device/phh/treble/gapps-go.mk)
 
-$(call inherit-product, device/phh/treble/aosp.mk)
 
-PRODUCT_NAME := aosp_arm64_ab
+PRODUCT_NAME := treble_arm64_boN
 PRODUCT_DEVICE := phhgsi_arm64_ab
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Phh-Treble (ARM64 - A/B)
+PRODUCT_MODEL := Phh-Treble Go
 
-# AOSP Packages
-PRODUCT_PACKAGES += \
-    Launcher3 \
-    messaging \
-    Terminal
+PRODUCT_PACKAGES += 
