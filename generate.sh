677bf73b65a587af5311c4c99a9eb4ee5b4dd05f
Pierre-Hugues Husson <phh@phh.me>
2018-08-20 22:24:32 +0200

    Add option to pass a path to an mk in generate.sh

--- generate.sh
+++ generate.sh
@@ -3,11 +3,8 @@
 rom_script=''
 if [ -n "$1" ];then
 	if echo "$1" | grep -qF /;then
-        rom_script=''
-        for i in "$@";do
-            rom_script="$rom_script"$'\n''$(call inherit-product, '$i')'
-        done
-    else
+		rom_script='$(call inherit-product, '$1')'
+	else
 		rom_script='$(call inherit-product, device/phh/treble/'$1'.mk)'
 	fi
 fi
@@ -16,7 +13,7 @@ echo 'PRODUCT_MAKEFILES := \' > AndroidProducts.mk
 
 for part in a ab;do
 	for apps in vanilla gapps foss gapps-go;do
-		for arch in arm64 arm a64;do
+		for arch in arm64 arm;do
 			for su in yes no;do
 				apps_suffix=""
 				apps_script=""
@@ -43,17 +40,14 @@ for part in a ab;do
 					apps_script=''
 					apps_name="vanilla"
 				fi
-				if [ "$arch" == "arm" ];then
-					vndk="vndk-binder32.mk"
-				fi
-				if [ "$arch" == "a64" ];then
-					vndk="vndk32.mk"
-				fi
+                if [ "$arch" == "arm" ];then
+                    vndk="vndk-binder32.mk"
+                fi
 
 				su_suffix='N'
 				if [ "$su" == "yes" ];then
 					su_suffix='S'
-					extra_packages+=' phh-su me.phh.superuser'
+					extra_packages+=' phh-su'
 				fi
 
 				part_suffix='a'
