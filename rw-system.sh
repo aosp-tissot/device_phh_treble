695472e9a6d422fbbb1de14ee54f2c1f0ee4802e
sooti <sooti85@gmail.com>
2019-10-28 16:59:17 +0100

    Doogee y8 HW encryption is broken, use software for now

--- rw-system.sh
+++ rw-system.sh
@@ -578,6 +578,15 @@ if [ $(find /vendor/etc/audio -type f |wc -l) -le 3 ];then
 	mount -o bind /mnt/phh/empty_dir /vendor/etc/audio || true
 fi
 
+if ! grep android.hardware.ir /vendor/manifest.xml;then
+    mount -o bind system/phh/empty /system/etc/permissions/android.hardware.consumerir.xml
+fi
+
+# Disable HW Encryption on Doogee Y8 for now
+if getprop ro.vendor.build.fingerprint | grep -iq -E -e '^DOOGEE/Y8'; then
+   for i in /vendor/lib/hw/keystore* /vendor/lib64/hw/keystore*;do mount /system/phh/empty $i;done
+fi
+
 for f in /vendor/lib{,64}/hw/com.qti.chi.override.so /vendor/lib{,64}/libVD*;do
     [ ! -f $f ] && continue
     # shellcheck disable=SC2010
