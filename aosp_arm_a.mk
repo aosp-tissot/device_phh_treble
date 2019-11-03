503d754a119f3c45dbb7186412c88a8344477e4f
sooti <sooti85@gmail.com>
2019-11-03 08:44:54 +0100

    fix arm32 boot

--- aosp_arm_a.mk
+++ aosp_arm_a.mk
@@ -1,8 +1,9 @@
 TARGET_GAPPS_ARCH := arm
 $(call inherit-product, device/phh/treble/base-pre.mk)
 include build/make/target/product/legacy_gsi_common.mk
-$(call inherit-product, vendor/vndk/vndk.mk)
+$(call inherit-product, vendor/vndk/vndk-binder32.mk)
 $(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, device/phh/treble/base-sas.mk)
 
 $(call inherit-product, device/phh/treble/aosp.mk)
 
