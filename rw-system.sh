5e45e9feffbb4ac776cd0593df97a8a4104e9e0b
sooti <sooti85@gmail.com>
2020-09-21 11:30:45 +0000

    Change product name for note 9s/pro/max to fix hotword

--- rw-system.sh
+++ rw-system.sh
@@ -91,18 +91,6 @@ if getprop ro.vendor.build.fingerprint |grep -qi -e redmi/curtana -e redmi/joyeu
     setprop persist.device_config.runtime_native.usap_pool_enabled true
     setprop debug.sf.latch_unsignaled 1
     setprop debug.sf.enable_gl_backpressure 1
-    if [ ! -f /system/phh/hotword_true ];then
-       mount -o remount,rw /
-       mount -o remount,rw /system
-       mv /system/phh/hotword/HotwordEnrollmentOKGoogleHEXAGON/HotwordEnrollmentOKGoogleHEXAGON.apk.tmp /system/phh/hotword/HotwordEnrollmentOKGoogleHEXAGON/HotwordEnrollmentOKGoogleHEXAGON.apk
-       mv /system/phh/hotword/HotwordEnrollmentXGoogleHEXAGON/HotwordEnrollmentXGoogleHEXAGON.apk.tmp /system/phh/hotword/HotwordEnrollmentXGoogleHEXAGON/HotwordEnrollmentXGoogleHEXAGON.apk
-       mv /system/phh/hotword/HotwordEnrollmentOKGoogleHEXAGON /system/product/priv-app/
-       mv /system/phh/hotword/HotwordEnrollmentXGoogleHEXAGON /system/product/priv-app/
-       chmod -R 0755 /system/product/priv-app/HotwordEnrollment*
-       touch /mnt/phh/hotword
-       mount -o remount,ro /
-       mount -o remount,ro /system
-    fi
 fi
 
 fixSPL() {
@@ -649,10 +637,10 @@ if getprop ro.vendor.build.fingerprint | grep -iq -E -e '^UMIDIGI/A7_Pro' -e '^D
    setprop ro.mtk_perf_simple_start_win 1
    setprop ro.mtk_perf_fast_start_win 1
    setprop ro.mtk_perf_response_time 1
-   setprop persist.sys.phh.ims.mtk true
-   setprop persist.dbg.volte_avail_ovr 1
-   setprop persist.dbg.vt_avail_ovr 1
-   setprop persist.dbg.wfc_avail_ovt 1
+#   setprop persist.sys.phh.ims.mtk true
+#   setprop persist.dbg.volte_avail_ovr 1
+#   setprop persist.dbg.vt_avail_ovr 1
+#   setprop persist.dbg.wfc_avail_ovt 1
 fi
 
 # Disable HW Encryption on Doogee Y8 for now
@@ -779,6 +767,17 @@ if [ -f /system/phh/secure ];then
     resetprop ro.secure 1
     resetprop ro.build.type user
     resetprop ro.build.selinux 0
+    // Fix the google hotword detection for android 11 on redmi note 9s
+    if getprop ro.vendor.build.fingerprint |grep -qi -e redmi/curtana;then
+       resetprop ro.product.model "Note 9S"
+    fi
+    if getprop ro.vendor.build.fingerprint |grep -qi -e redmi/joyeuse;then
+       resetprop ro.product.model "Note 9S Pro"
+    fi
+    if getprop ro.vendor.build.fingerprint |grep -qi -e redmi/excalibur;then
+       resetprop ro.product.model "Note 9S Pro Max"
+    fi
+    resetprop ro.adb.secure 1
 
     resetprop_phh ro.adb.secure 1
     setprop ctl.restart adbd
@@ -981,6 +980,7 @@ if getprop ro.bionic.cpu_variant |grep -q kryo300;then
 fi
 
 resetprop_phh ro.control_privapp_permissions log
+
 
 if [ -f /vendor/etc/init/vendor.ozoaudio.media.c2@1.0-service.rc ];then
     if [ "$vndk" -le 29 ]; then
