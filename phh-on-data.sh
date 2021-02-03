f97a08994617a044ac643b54fad9659c95f4f00b
sooti <sooti85@gmail.com>
2021-02-03 12:51:10 +0200

    Re-enable fuse, it breaks google photos when its off

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -66,6 +66,10 @@ if [ "$vndk" = 28 ];then
     mount $minijailSrc /vendor/lib/libminijail.so
 fi
 
+if getprop persist.sys.fflag.override.settings_fuse|grep -q -e false;then
+   setprop persist.sys.fflag.override.settings_fuse true
+fi
+
 # Enable IMS on qcom devices
 if getprop ro.boot.hardware|grep -q -e qcom;then
     if getprop ro.product.cpu.abi | grep -q -e 'arm64-v8a'; then
