d19e6bf4a03e7fae1ac753220bad42c7f9316581
sooti <sooti85@gmail.com>
2021-06-14 18:12:57 +0300

    move the curtana props to on data

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -29,3 +29,15 @@ if [ "$vndk" = 28 ];then
     mount $minijailSrc64 /vendor/lib64/libminijail.so
     mount $minijailSrc /vendor/lib/libminijail.so
 fi
+if getprop ro.vendor.build.fingerprint |grep -iq  -e redmi/curtana \
+    -e redmi/joyeuse;then
+    setprop persist.sys.phh.disable_a2dp_offload true
+    setprop persist.bluetooth.bluetooth_audio_hal.disabled true
+    setprop persist.sys.phh.caf.audio_policy 1
+    mount -o bind /system/etc/mixer_paths_wcd937x.xml /vendor/etc/mixer_paths_wcd937x.xml
+    mount -o bind /system/etc/media_profiles_vendor.xml /vendor/etc/media_profiles_vendor.xml
+    setprop ctl.restart vendor.audio-hal-2-0
+    setprop persist.sys.phh.linear_brightness true
+    pkill -f com.android.bluetooth
+fi
+
