b03d5577c6477e00287980e76f7f8c4113fca908
sooti <sooti85@gmail.com>
2021-06-14 18:17:14 +0300

    remove bloat apps to reduce size

--- overrides/Android.mk
+++ overrides/Android.mk
@@ -8,6 +8,13 @@ PACKAGES.$(LOCAL_MODULE).OVERRIDES := \
 	YouTubeGo \
 	DuoGo \
 	Traceur \
+        AssistantGo \
+        GalleryGo \
+        GMailGo \
+        GoogleSearchGo \
+        MapsGo \
+        NavGo \
+        YouTubeGo \
 	GmsSampleIntegration \
 	GmsSampleIntegrationGo \
 	GmsSampleIntegrationGo2GB \
@@ -37,13 +44,18 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := phh-gapps-overrides
 PACKAGES.$(LOCAL_MODULE).OVERRIDES := \
+	AssistantShell \
+	Drive \
+	Gmail \
 	Calendar \
+	Photos \
+	Duo \
+	YouTube \
+	Maps \
 	YTMusic \
 	Videos \
 	Traceur \
 	FilesGoogle \
-	AssistantShell \
-	Wellbeing \
 	GmsSampleIntegration \
 	GmsSampleIntegrationGo \
 	GmsSampleIntegrationGo2GB \
