b044e8c13ef72be1820cbe5066ddf2fc4a1cd9ed
sooti <sooti85@gmail.com>
2021-06-14 18:16:50 +0300

    Remove Hotword apps for now and fix mismerge

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -13,22 +13,6 @@ if getprop persist.sys.phh.caf.media_profile |grep -q true;then
     setprop media.settings.xml "/vendor/etc/media_profiles_vendor.xml"
 fi
 
-
-minijailSrc=/system/system_ext/apex/com.android.vndk.v28/lib/libminijail.so
-minijailSrc64=/system/system_ext/apex/com.android.vndk.v28/lib64/libminijail.so
-if [ "$vndk" = 27 ];then
-    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
-    mount $minijailSrc /vendor/lib/libminijail_vendor.so
-fi
-
-if [ "$vndk" = 28 ];then
-    mount $minijailSrc64 /vendor/lib64/libminijail_vendor.so
-    mount $minijailSrc /vendor/lib/libminijail_vendor.so
-    mount $minijailSrc64 /system/lib64/vndk-28/libminijail.so
-    mount $minijailSrc /system/lib/vndk-28/libminijail.so
-    mount $minijailSrc64 /vendor/lib64/libminijail.so
-    mount $minijailSrc /vendor/lib/libminijail.so
-fi
 if getprop ro.vendor.build.fingerprint |grep -iq  -e redmi/curtana \
     -e redmi/joyeuse -e redmi/excalibur;then
     setprop persist.sys.phh.disable_a2dp_offload true
@@ -37,7 +21,7 @@ if getprop ro.vendor.build.fingerprint |grep -iq  -e redmi/curtana \
     mount -o bind /system/etc/mixer_paths_wcd937x.xml /vendor/etc/mixer_paths_wcd937x.xml
     mount -o bind /system/etc/media_profiles_vendor.xml /vendor/etc/media_profiles_vendor.xml
     setprop ctl.restart vendor.audio-hal-2-0
-    setprop persist.sys.phh.linear_brightness true
+    setprop persist.sys.phh.linear_brightness false
     pkill -f com.android.bluetooth
 fi
 
