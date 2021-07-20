28afaa2425fdb0be04d97a19cc2b4f449e72680f
sooti <sooti85@gmail.com>
2021-07-20 13:57:07 +0300

    add redmi note 9s/pro/max persist props

--- phh-on-data.sh
+++ phh-on-data.sh
@@ -37,6 +37,19 @@ if getprop persist.sys.phh.caf.media_profile |grep -q true;then
     setprop media.settings.xml "/vendor/etc/media_profiles_vendor.xml"
 fi
 
+# Add persist props for Redmi Note 9S/Pro/Max
+if getprop ro.vendor.build.fingerprint |grep -iq  -e redmi/curtana \
+    -e redmi/joyeuse -e redmi/excalibur;then
+    setprop persist.sys.phh.disable_a2dp_offload true
+    setprop persist.bluetooth.bluetooth_audio_hal.disabled true
+    setprop persist.sys.phh.caf.audio_policy 1
+    mount -o bind /system/etc/mixer_paths_wcd937x.xml /vendor/etc/mixer_paths_wcd937x.xml
+    mount -o bind /system/etc/media_profiles_vendor.xml /vendor/etc/media_profiles_vendor.xml
+    setprop ctl.restart vendor.audio-hal-2-0
+    setprop persist.sys.phh.linear_brightness false
+    setprop persist.sys.phh.backlight.scale 1
+    pkill -f com.android.bluetooth
+fi
 
 minijailSrc=/system/system_ext/apex/com.android.vndk.v28/lib/libminijail.so
 minijailSrc64=/system/system_ext/apex/com.android.vndk.v28/lib64/libminijail.so
